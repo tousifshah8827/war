d798ee22d377cae0992cdfb575133f0e5f3fb060
tousif <tousif@gmail.com>
2023-09-02 14:14:35 +0530

    tousif maven project

---
+++ src/test/java/tousif/shah/ShahApplicationTests.java
@@ -0,0 +1,13 @@
+package tousif.shah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShahApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
